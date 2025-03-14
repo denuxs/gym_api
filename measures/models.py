de5344caae43de0a10371b795225ca134b312fa2
Denis López <denuxs@gmail.com>
2025-03-14 11:13:08 -0600

    refactor measure app

--- measures/models.py
+++ measures/models.py
@@ -1,9 +1,10 @@
-from email import header
+from images.models import Image
 from django.db import models
 
 from django.contrib.auth import get_user_model
 import os
 from uuid import uuid4
+from django.contrib.contenttypes.fields import GenericRelation
 
 User = get_user_model()
 
@@ -35,57 +36,60 @@ class Measure(models.Model):
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        # related_name="equipment_catalog_set",
     )
     comment = models.TextField(null=True, blank=True)
 
-    weight = models.FloatField(default=0)
-    height = models.FloatField(default=0)
+    # weight = models.FloatField(default=0)
+    # height = models.FloatField(default=0)
 
-    chest = models.FloatField(default=0)
-    abdomen = models.FloatField(default=0)
-    back = models.FloatField(default=0)
+    measures = models.JSONField(null=True, blank=True)
 
-    arm_left = models.FloatField(default=0)
-    arm_right = models.FloatField(default=0)
+    # chest = models.FloatField(default=0)
+    # abdomen = models.FloatField(default=0)
+    # back = models.FloatField(default=0)
 
-    forearm = models.FloatField(default=0)
+    # arm_left = models.FloatField(default=0)
+    # arm_right = models.FloatField(default=0)
 
-    leg_left = models.FloatField(default=0)
-    leg_right = models.FloatField(default=0)
+    # forearm = models.FloatField(default=0)
 
-    waist = models.FloatField(default=0)
-    hips = models.FloatField(default=0)
+    # leg_left = models.FloatField(default=0)
+    # leg_right = models.FloatField(default=0)
 
-    glutes = models.FloatField(default=0)
+    # waist = models.FloatField(default=0)
+    # hips = models.FloatField(default=0)
 
-    photo_back = models.ImageField(
-        upload_to=measure_directory_path,
-        default="default.jpg",
-        blank=True,
-        null=True,
-    )
+    # glutes = models.FloatField(default=0)
 
-    photo_front = models.ImageField(
-        upload_to=measure_directory_path,
-        default="default.jpg",
-        blank=True,
-        null=True,
-    )
+    images = GenericRelation(Image)
 
-    photo_left = models.ImageField(
-        upload_to=measure_directory_path,
-        default="default.jpg",
-        blank=True,
-        null=True,
-    )
+    # photo_back = models.ImageField(
+    #     upload_to=measure_directory_path,
+    #     default="default.jpg",
+    #     blank=True,
+    #     null=True,
+    # )
 
-    photo_right = models.ImageField(
-        upload_to=measure_directory_path,
-        default="default.jpg",
-        blank=True,
-        null=True,
-    )
+    # photo_front = models.ImageField(
+    #     upload_to=measure_directory_path,
+    #     default="default.jpg",
+    #     blank=True,
+    #     null=True,
+    # )
+
+    # photo_left = models.ImageField(
+    #     upload_to=measure_directory_path,
+    #     default="default.jpg",
+    #     blank=True,
+    #     null=True,
+    # )
+
+    # photo_right = models.ImageField(
+    #     upload_to=measure_directory_path,
+    #     default="default.jpg",
+    #     blank=True,
+    #     null=True,
+    # )
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
